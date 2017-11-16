fd6608cb242708e4476d9719f1aae32c86980ee6
Romain DESMIDT <romain.desmidt@gmail.com>
2017-11-16 12:36:06 +0100

    project kelvin initial commit

---
+++ projectkelvin.js
@@ -0,0 +1,13 @@
+//const kelvin
+const kelvin = 294;
+//const celsius
+const celsius = kelvin - 273;
+// stdout
+console.log(kelvin);
+console.log(celsius);
+// var fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+console.log(fahrenheit);
+// Fahrenheit = Celsius * (9/5) + 32
+fahrenheit = Math.floor(fahrenheit);
+console.log(fahrenheit);
